454e5a9d578379125889b721ef17b8a4a7476026
Mike van Oorschot <michael.oorschot@codurance.com>
2022-11-17 08:10:08 +0000

    Added more complex rate limit test.

--- PollyTestDrive.Tests/RateLimitTests.cs
+++ PollyTestDrive.Tests/RateLimitTests.cs
@@ -70,4 +70,59 @@ public class RateLimitTests
         _outputHelper.WriteLine($"[{sw.Elapsed}] Rate limit rejected {rejectedRequestCount} out of {requestCount} total requests");
         _outputHelper.WriteLine($"[{sw.Elapsed}] Completed {completedRequetsCount} out of {requestCount} total requests");
     }
+    
+    [Fact]
+    public async Task Maximum_10_Executing_In_1_Second_Add_5_Every_300_Milliseconds()
+    {
+        int requestCount = 0;
+        int rejectedRequestCount = 0;
+        int completedRequetsCount = 0;
+        Stopwatch sw = new();
+        sw.Start();
+        
+        var rateLimitPolicy = Policy
+            .RateLimitAsync(
+                numberOfExecutions: 10, 
+                perTimeSpan: TimeSpan.FromSeconds(1), 
+                maxBurst: 5, // to allow 5 requests to be started at once
+                retryAfterFactory: (retryAfter, context) =>
+                {
+                    _outputHelper.WriteLine($"[{sw.Elapsed}] Rate limit rejected request, should retry after {retryAfter}");
+                    Interlocked.Increment(ref rejectedRequestCount);
+                    return Task.CompletedTask;
+                });
+        
+        var tasks = new List<Task>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                requestCount++;
+                tasks.Add(
+                    rateLimitPolicy.ExecuteAsync(
+                        async (cancellationToken) =>
+                        {
+                            var client = _serviceProvider.GetRequiredService<DelayClient>();
+                            await client.Execute(1000, cancellationToken);
+                            Interlocked.Increment(ref completedRequetsCount);
+                            _outputHelper.WriteLine($"[{sw.Elapsed}] Request completed");
+                            return Task.CompletedTask;
+                        },
+                        CancellationToken.None)
+                );
+            }
+            
+            await Task.Delay(300);
+        }
+
+        _outputHelper.WriteLine($"[{sw.Elapsed}] Created {requestCount} requests");
+        
+        Func<Task> act = () => Task.WhenAll(tasks);
+
+        await act.Should().NotThrowAsync();
+        
+        _outputHelper.WriteLine($"[{sw.Elapsed}] Rate limit rejected {rejectedRequestCount} out of {requestCount} total requests");
+        _outputHelper.WriteLine($"[{sw.Elapsed}] Completed {completedRequetsCount} out of {requestCount} total requests");
+    }
 }
